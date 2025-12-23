a0758c30b7f7b6dc5f4263cb2d03195dbbade520
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-23 11:50:14 -0600

    initial commit

---
+++ src/main/java/com/project/community/community_security_service/CommunitySecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.community.community_security_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitySecurityServiceApplication.class, args);
+	}
+
+}
