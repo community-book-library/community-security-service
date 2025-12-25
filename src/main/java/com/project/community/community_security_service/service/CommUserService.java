fa96e81b0db65fc72035d485e2c38f1c093a5fba
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-25 11:31:29 -0600

    register user endpoint service

--- src/main/java/com/project/community/community_security_service/service/CommUserService.java
+++ src/main/java/com/project/community/community_security_service/service/CommUserService.java
@@ -1,7 +1,6 @@
 package com.project.community.community_security_service.service;
 
 import com.project.community.community_security_service.DTO.UserDTO;
-import com.project.community.community_security_service.entity.LoginInfo;
 import com.project.community.community_security_service.entity.Roles;
 import com.project.community.community_security_service.entity.UserAuth;
 import com.project.community.community_security_service.entity.Users;
@@ -9,10 +8,13 @@ import com.project.community.community_security_service.repository.CommRoleRepos
 import com.project.community.community_security_service.repository.CommUserAuthRepository;
 import com.project.community.community_security_service.repository.CommUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,10 +29,14 @@ public class CommUserService {
 
     @Autowired
     private CommRoleRepository commRoleRepository;
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+
+    @Value("${app.title}")
+    private String appTitle;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
     public void registerUser(UserDTO userDTO){
@@ -38,12 +44,10 @@ public class CommUserService {
 
         List<Roles> roles = commRoleRepository.findAll();
         Roles role = roles.stream()
-                .filter(r -> r.getRole().name().equals(Roles.Role.USER.name()))
-                .findFirst().get();
-
+                .filter(r -> r.getRole().equals("USER")).findFirst().get();
         user.setFirstName(userDTO.getFirstName());
         user.setLastName(userDTO.getLastName());
-        user.setCreatedBy("application");
+        user.setCreatedBy(appTitle);
         LocalDateTime dt = LocalDateTime.now();
         user.setCreatedTimestamp(dt);
         user.setRoles(role);
@@ -51,17 +55,10 @@ public class CommUserService {
         UserAuth userAuth = new UserAuth();
         userAuth.setUser(user);
         userAuth.setUsername(userDTO.getUsername());
-//        PasswordEncoder passEncoder = passwordEncoder();
-        userAuth.setPassword(userDTO.getPassword());
-        userAuth.setCreatedBy("application");
+        PasswordEncoder passEncoder = passwordEncoder();
+        userAuth.setPassword(passEncoder.encode(userDTO.getPassword()));
+        userAuth.setCreatedBy(appTitle);
         userAuth.setCreatedTimestamp(dt);
-        LoginInfo loginInfo = new LoginInfo();
-        loginInfo.setLoginAttempt(1);
-        loginInfo.setLoginStatus(LoginInfo.LoginStatus.CREATED);
-        loginInfo.setLastLoginTime(dt);
-        List<LoginInfo> list = new ArrayList<>();
-        list.add(loginInfo);
-        userAuth.setLoginInfo(list);
         commUserAuthRepository.save(userAuth);
     }
 }
