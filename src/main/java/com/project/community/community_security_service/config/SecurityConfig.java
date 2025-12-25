09efc9b5c591c83ff26eb21122449b4bbbb7ccc8
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-25 11:27:24 -0600

    Added register endpoint

--- src/main/java/com/project/community/community_security_service/config/SecurityConfig.java
+++ src/main/java/com/project/community/community_security_service/config/SecurityConfig.java
@@ -1,26 +1,31 @@
 package com.project.community.community_security_service.config;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                // Disable CSRF (not needed for stateless JWT)
-//                .csrf(csrf -> csrf.disable())
-//
-//                // Configure endpoint authorization
-//                .authorizeHttpRequests(auth -> auth
-//                        // Public endpoints
-//                        .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
-//
-//                        // All other endpoints require authentication
-//                        .anyRequest().authenticated()
-//                )
-//
-//                // Stateless session (required for JWT)
-//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
-//        return http.build();
-//    }
-//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Disable CSRF (not needed for stateless JWT)
+                .csrf(csrf -> csrf.disable())
+
+                // Configure endpoint authorization
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // Stateless session (required for JWT)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
