35cdcfd9f29c2b786ca631f743960a1628ca34c2
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-25 19:31:48 -0600

    Added DTO and UserDetails for login

---
+++ src/main/java/com/project/community/community_security_service/DTO/AuthDTO.java
@@ -0,0 +1,13 @@
+package com.project.community.community_security_service.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthDTO {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/project/community/community_security_service/service/CommUserDetails.java
@@ -0,0 +1,66 @@
+package com.project.community.community_security_service.service;
+
+import com.project.community.community_security_service.entity.LoginInfo;
+import com.project.community.community_security_service.entity.Roles;
+import com.project.community.community_security_service.entity.UserAuth;
+import com.project.community.community_security_service.entity.Users;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CommUserDetails implements UserDetails {
+    private String username;
+    private String password;
+    private Users user;
+    private boolean enabled;
+    private List<LoginInfo> loginInfo;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CommUserDetails(UserAuth userAuth) {
+        this.username = userAuth.getUsername();
+        this.password = userAuth.getPassword();
+        this.user = userAuth.getUser();
+        this.loginInfo = userAuth.getLoginInfo();
+        this.enabled = true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Roles role = user.getRoles();
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
+        return List.of(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
