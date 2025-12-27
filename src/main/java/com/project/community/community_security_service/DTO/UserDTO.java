d950c20f1ef58dc687d1ad359b3f49e7a3a2259d
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-26 21:14:10 -0600

    Updated input by adding email

--- src/main/java/com/project/community/community_security_service/DTO/UserDTO.java
+++ src/main/java/com/project/community/community_security_service/DTO/UserDTO.java
@@ -1,5 +1,6 @@
 package com.project.community.community_security_service.DTO;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
-
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
+    private String email;
 
 }
