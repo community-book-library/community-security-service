9ba0bbeb72182afae4ae8ea7108b9613112ac433
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-27 21:11:07 -0600

    updated DBschema

--- src/main/resources/securityDBschema.sql
+++ src/main/resources/securityDBschema.sql
@@ -1,20 +1,16 @@
-drop table communitylibrary.users;
-drop table communitylibrary.login_info;
-drop table communitylibrary.roles;
 drop table communitylibrary.user_auth;
+drop table communitylibrary.users;
+drop table communitylibrary.roles;
+
 
 create table communitylibrary.roles(role_id BIT(10) primary key,role varchar(50));
 
-create table communitylibrary.login_info(id int(255) AUTO_INCREMENT PRIMARY KEY,last_login_time TIMESTAMP,
-login_status enum("CREATED","ACTIVE","LOCKED","DISABLED"),login_attempt BIT(10),user_id smallint, foreign key(user_id) references 
-communitylibrary.users(id));
-
-CREATE TABLE communitylibrary.users(id smallint AUTO_INCREMENT PRIMARY KEY,first_name varchar(50),last_name varchar(50), role_id BIT(10),
-foreign key(role_id) references communitylibrary.roles(role_id),created_timestamp TIMESTAMP,updated_timestamp TIMESTAMP,
-created_by varchar(50),updated_by varchar(50));
+CREATE TABLE communitylibrary.users(id smallint AUTO_INCREMENT PRIMARY KEY,first_name varchar(50),last_name varchar(50),
+email varchar(255), role_id BIT(10),foreign key(role_id) references communitylibrary.roles(role_id),created_timestamp TIMESTAMP,
+updated_timestamp TIMESTAMP,created_by varchar(50),updated_by varchar(50));
 
 create table communitylibrary.user_auth(id smallint AUTO_INCREMENT PRIMARY KEY, user_id smallint, foreign key(user_id) references 
-communitylibrary.users(id), username varchar(50), password varchar(255),created_timestamp TIMESTAMP,updated_timestamp TIMESTAMP,created_by varchar(50),updated_by 
+communitylibrary.users(id), username varchar(50), login_status enum("CREATED","ACTIVE","LOCKED","DISABLED"),invalid_login_attempt BIT(10), password varchar(255),created_timestamp TIMESTAMP,updated_timestamp TIMESTAMP,created_by varchar(50),updated_by 
 varchar(50));
 
 insert into communitylibrary.roles values(1,"USER");
