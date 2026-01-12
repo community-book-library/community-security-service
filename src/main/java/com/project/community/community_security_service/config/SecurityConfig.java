cc705327d329e306c1efb042f7e3de169a04c533
msiva06 <m.sivasubramanian06@gmail.com>
2026-01-11 21:31:01 -0600

    added MFAAuthenticationProvider in provider list in AuthenticationManager

--- src/main/java/com/project/community/community_security_service/config/SecurityConfig.java
+++ src/main/java/com/project/community/community_security_service/config/SecurityConfig.java
@@ -2,12 +2,13 @@ package com.project.community.community_security_service.config;
 
 import com.project.community.community_security_service.filter.JWTAuthFilter;
 import com.project.community.community_security_service.service.CommUserDetailsService;
+import com.project.community.community_security_service.service.MFAAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +29,7 @@ public class SecurityConfig {
     @Autowired
     private JWTAuthFilter jwtAuthFilter;
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -34,8 +38,7 @@ public class SecurityConfig {
 
                 // Configure endpoint authorization
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login" ).permitAll()
-                        .requestMatchers("/auth/send-otp", "/auth/verify-otp").hasRole("USER")
+                        .requestMatchers("/auth/register", "/auth/login", "/auth/admin","/auth/verify-otp", "/auth/resend-otp" ).permitAll()
                         .anyRequest().authenticated()
                 )
                 // Stateless session (required for JWT)
@@ -46,9 +49,15 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
-            throws Exception {
-        return config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(
+            DaoAuthenticationProvider daoAuthenticationProvider,
+            MFAAuthenticationProvider mfaAuthenticationProvider) {
+
+        // Create ProviderManager with both authentication providers
+        return new ProviderManager(Arrays.asList(
+                daoAuthenticationProvider,
+                mfaAuthenticationProvider
+        ));
     }
 
     @Bean
