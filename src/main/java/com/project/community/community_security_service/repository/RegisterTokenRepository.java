86b5e18e05247a8a592a69af9d4b2b2a48bd1c21
msiva06 <m.sivasubramanian06@gmail.com>
2026-01-11 21:30:07 -0600

    Updated to Optional return type

--- src/main/java/com/project/community/community_security_service/repository/RegisterTokenRepository.java
+++ src/main/java/com/project/community/community_security_service/repository/RegisterTokenRepository.java
@@ -4,6 +4,8 @@ package com.project.community.community_security_service.repository;
 import com.project.community.community_security_service.entity.RegisterToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RegisterTokenRepository extends JpaRepository<RegisterToken,Integer> {
-    RegisterToken findByToken(String token);
+    Optional<RegisterToken> findByToken(String token);
 }
